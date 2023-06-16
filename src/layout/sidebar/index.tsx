8ae1f3bd795322242132c6c99869a8454f197b35
Luis Souza <luisotavioldesouza1+1@gmail.com>
2023-06-16 16:19:42 -0300

    feature: adding icons and updating sidebar

--- src/layout/sidebar/index.tsx
+++ src/layout/sidebar/index.tsx
@@ -1,16 +1,20 @@
 import { HiOutlineCurrencyDollar, HiOutlineTag, HiOutlineUserGroup } from "react-icons/hi";
 import MenuItem from "../../components/menu-item";
+import logoUrl from '../../assets/logo-white.svg';
+import { Link } from "react-router-dom";
 
 function Sidebar() {
 
     return (
-        <aside className="row-span-full col-span-1 bg-[#052636] border-r border-r-[#084969] text-white flex flex-col">
-            <div className="w-full h-1/6">
-                Logo here
-            </div>
-            <MenuItem path="/expenses" isActive={false}>  <HiOutlineCurrencyDollar /> Expenses </MenuItem>
-            <MenuItem path="/expenses-categories" isActive={false}> <HiOutlineTag /> Expenses Categories </MenuItem>
-            <MenuItem path="/residents" isActive={false}> <HiOutlineUserGroup/> Residents </MenuItem>
+        <aside className="row-span-full col-span-1 bg-[#052636] border-r border-r-[#304242] text-white flex flex-col">
+            <Link to="/" className="w-10/12 h-[9.9%] mx-auto flex items-center">
+                <img src={logoUrl} style={{
+                    fill: "white"
+                }}/>
+            </Link>
+            <MenuItem path="/expenses" isActive={false}>  <HiOutlineCurrencyDollar /> expenses </MenuItem>
+            <MenuItem path="/expenses-categories" isActive={false}> <HiOutlineTag /> expenses categories </MenuItem>
+            <MenuItem path="/residents" isActive={false}> <HiOutlineUserGroup/> residents </MenuItem>
         </aside>
     )
 }
