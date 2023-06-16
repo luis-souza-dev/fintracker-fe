da176b00f113b6243051cf414052088f9fff84f5
Luis Souza <luisotavioldesouza1+1@gmail.com>
2023-06-16 16:21:29 -0300

    feature: main-panel topbar state set up

--- src/layout/topbar/index.tsx
+++ src/layout/topbar/index.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from "react";
+import { HiOutlineChartBar, HiOutlineCurrencyDollar, HiOutlineTag, HiOutlineUserGroup } from "react-icons/hi";
+import { useLocation } from "react-router-dom";
 
 function Topbar() {
+    const location = useLocation();
+
+    const [state, setState] = useState('');
+
+    useEffect(() => {
+        const parsedName = location.pathname.replace(/\//,'').replace(/-/, ' ');
+        setState(parsedName == '' ? 'Dashboard' : parsedName);
+    }, [location]);
 
     return (
-        <header className="w-full h-24 bg-[#052636] border-b border-b-[#084969] text-white flex flex-col">
-           I'ma header
+        <header className="w-full h-[10%] bg-[#052636] border-b border-b-[#304242] text-white flex text-3xl">
+           <span className="self-center ml-5 flex gap-x-4 items-center"> { getIcon(state) } { state } </span>
         </header>
     )
+}
+
+const getIcon = (location: string) : React.JSX.Element => {
+
+    switch (location) {
+        case 'expenses':
+            return <HiOutlineCurrencyDollar />;
+        case 'expenses categories':
+            return <HiOutlineTag />
+        case 'residents':
+                return <HiOutlineUserGroup />
+        default:
+            return <HiOutlineChartBar/>
+    }
+
 }
 
 export default Topbar
