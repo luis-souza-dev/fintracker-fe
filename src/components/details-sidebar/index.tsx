384ca10cbea621b1335cea882328ef093cf804a8
Luis Souza <luisotavioldesouza1+1@gmail.com>
2023-06-16 16:21:02 -0300

    feature: adding table and details sidebar

---
+++ src/components/details-sidebar/index.tsx
@@ -0,0 +1,13 @@
+function DetailsSidebar() {
+
+    return (
+      <>
+        <div className="w-3/12 h-5/6 border border-[#304242] rounded-lg">
+                testing
+        </div>
+      </>
+    )
+}
+
+export default DetailsSidebar;
+  
---
+++ src/components/table/index.tsx
@@ -0,0 +1,15 @@
+import { NavLink } from "react-router-dom";
+
+function Table() {
+
+    return (
+      <>
+        <div className="w-8/12 h-5/6 border border-[#304242] rounded-lg">
+                testing table
+        </div>
+      </>
+    )
+}
+
+export default Table;
+  
